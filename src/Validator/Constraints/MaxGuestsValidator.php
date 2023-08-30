fd6e70d60cc7ad0a1dce54d25ea5329feac9ba81
VibroMatvey <pooloff2004@gmail.com>
2023-08-30 23:59:05 +0400

    fix #4

--- src/Validator/Constraints/MaxGuestsValidator.php
+++ src/Validator/Constraints/MaxGuestsValidator.php
@@ -2,8 +2,10 @@
 
 namespace App\Validator\Constraints;
 
+use App\Entity\GuestList;
 use App\Repository\GuestListRepository;
 use App\Repository\TablesRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
@@ -14,10 +16,17 @@ final class MaxGuestsValidator extends ConstraintValidator
     }
     public function validate($value, Constraint $constraint): void
     {
-        $guests = $this->guestListRepository->findBy(['tables' => $value]);
-        $maxGuest = $this->tablesRepository->find($value)->getMaxGuests();
-        if (count($guests) === $maxGuest) {
-            $this->context->buildViolation($constraint->message)->addViolation();
+        $table = $this->tablesRepository->find($value);
+        if ($this->context->getObject()->getId()) {
+            $guest = $this->guestListRepository->find($this->context->getObject()->getId());
+            $table->addTable($guest);
+            if ($table->getMaxGuests() < count($table->getTables())) {
+                $this->context->buildViolation($constraint->message)->addViolation();
+            }
+        } else {
+            if ($table->getMaxGuests() == count($table->getTables())) {
+                $this->context->buildViolation($constraint->message)->addViolation();
+            }
         }
     }
 }
