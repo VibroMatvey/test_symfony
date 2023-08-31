02263a3ff0c54ae20d1f7dd1a221758411e79b0f
VibroMatvey <pooloff2004@gmail.com>
2023-08-31 11:25:06 +0400

    #5

--- src/Validator/Constraints/MaxGuestsValidator.php
+++ src/Validator/Constraints/MaxGuestsValidator.php
@@ -17,15 +17,17 @@ final class MaxGuestsValidator extends ConstraintValidator
     public function validate($value, Constraint $constraint): void
     {
         $table = $this->tablesRepository->find($value);
-        if ($this->context->getObject()->getId()) {
-            $guest = $this->guestListRepository->find($this->context->getObject()->getId());
-            $table->addTable($guest);
-            if ($table->getMaxGuests() < count($table->getTables())) {
-                $this->context->buildViolation($constraint->message)->addViolation();
-            }
-        } else {
-            if ($table->getMaxGuests() == count($table->getTables())) {
-                $this->context->buildViolation($constraint->message)->addViolation();
+        if ($table->getMaxGuests()) {
+            if ($this->context->getObject()->getId()) {
+                $guest = $this->guestListRepository->find($this->context->getObject()->getId());
+                $table->addTable($guest);
+                if ($table->getMaxGuests() < count($table->getTables())) {
+                    $this->context->buildViolation($constraint->message)->addViolation();
+                }
+            } else {
+                if ($table->getMaxGuests() == count($table->getTables())) {
+                    $this->context->buildViolation($constraint->message)->addViolation();
+                }
             }
         }
     }
