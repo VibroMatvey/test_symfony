a377c3c51d84dc816863207e929d9c3979b42e24
VibroMatvey <pooloff2004@gmail.com>
2023-09-06 18:08:54 +0400

    migrations

--- migrations/Version20230906140553.php
+++ migrations/Version20230906140553.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230825165626 extends AbstractMigration
+final class Version20230906140553 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,9 @@ final class Version20230825165626 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `admin` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_880E0D76F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE guest_list (id INT AUTO_INCREMENT NOT NULL, tables_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, is_present TINYINT(1) DEFAULT NULL, INDEX IDX_6072A54585405FD2 (tables_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tables (id INT AUTO_INCREMENT NOT NULL, num INT NOT NULL, description VARCHAR(255) DEFAULT NULL, max_guests INT DEFAULT NULL, UNIQUE INDEX UNIQ_84470221DC43AF6E (num), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE guest_list ADD CONSTRAINT FK_6072A54585405FD2 FOREIGN KEY (tables_id) REFERENCES tables (id)');
     }
 
@@ -28,6 +30,8 @@ final class Version20230825165626 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE guest_list DROP FOREIGN KEY FK_6072A54585405FD2');
+        $this->addSql('DROP TABLE `admin`');
         $this->addSql('DROP TABLE guest_list');
+        $this->addSql('DROP TABLE tables');
     }
 }
